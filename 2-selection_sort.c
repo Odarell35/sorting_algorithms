5670b6a7e0a325798c0474251d1433076de41da5
Odarell35 <leratomawash0@gmail.com>
2023-07-21 07:45:59 -0700

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * selection_sort - selects the smallest element from an unsorted list
+ * in each iteration and places that element at
+ * the beginning of the unsorted list.
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, store, k, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		store = i;
+		for (k = i + 1; k < size; k++)
+		{
+			if (array[k] < array[store])
+				store = k;
+		}
+		temp = array[store];
+		array[store] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
+}
