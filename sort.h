e9146ab48dde5fa3fc183f04148a97a5b00106d5
siphoPrince <mabirimisesipho@gmail.com>
2023-07-22 00:19:46 -0700

    sort.h fix

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ typedef struct listint_s
 void swap(int *a, int *b);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
-void quick_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size);
 /*--------------------------------------------------*/	
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
