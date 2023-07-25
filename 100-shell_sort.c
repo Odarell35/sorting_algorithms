bbb73b774c91a19d2b87cf16f1528b5383fd6436
Odarell35 <leratomawash0@gmail.com>
2023-07-25 03:49:19 -0700

    fixed shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,25 +9,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t interval, i, j;
-	int temp;
+size_t interval, i, j;
+int temp;
 
-	interval = 1;
-	if (size < 2)
-		return;
-	while (interval < (size / 3))
-		interval = interval * 3 + 1;
-	for (interval = interval; interval > 0; interval /= 3)
+interval = 1;
+if (size < 2)
+	return;
+while (interval < (size / 3))
+	interval = interval * 3 + 1;
+for (interval = interval; interval > 0; interval /= 3)
+{
+for (i = interval; i < size; i += 1)
 	{
-		for (i = interval; i < size; i += 1)
-		{
-			temp = array[i];
-		for (j = i; j >= (interval - 1) && array[j - interval] >= temp; j -= interval)
-		{
-			array[j] = array[j - interval];
-		}
-			array[j] = temp;
-		}
-			print_array(array, size);
+	temp = array[i];
+	for (j = i; j >= (interval - 1) && array[j - interval] > temp; j -= interval)
+	{
+		array[j] = array[j - interval];
 	}
+		array[j] = temp;
+	}
+		print_array(array, size);
+}
 }
