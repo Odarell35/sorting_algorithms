51bcdde5786ee57fb97ada2f3d95b615a9f26b9a
Odarell35 <leratomawash0@gmail.com>
2023-07-25 03:21:00 -0700

    fixing sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -22,12 +22,12 @@ void shell_sort(int *array, size_t size)
 		for (i = interval; i < size; i += 1)
 		{
 			temp = array[i];
-			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+			for (j = i; j > (interval - 1) && array[j - interval] >= temp; j -= interval)
 			{
 				array[j] = array[j - interval];
 			}
 			array[j] = temp;
-			print_array(array, size);
 		}
+			print_array(array, size);
 	}
 }
