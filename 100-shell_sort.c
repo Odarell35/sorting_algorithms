6409be0ba67bfc3bb09799a1cdb34c2074cdd3b0
Odarell35 <leratomawash0@gmail.com>
2023-07-25 03:14:30 -0700

    shell sort fixing

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,22 +2,7 @@
 #include <math.h>
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- *@x: number
- *@y: number
- *Return: y < 0 return -1
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	else
-		return (x * _pow_recursion(x, y - 1));
-}
-/**
- * shell-sort - in-place comparision
+ * shell_sort - in-place comparision
  * @array: array to be sorted
  * @size: size of array
  * Return: void
@@ -25,18 +10,19 @@ int _pow_recursion(int x, int y)
 void shell_sort(int *array, size_t size)
 {
 	size_t interval, i, j;
-	int temp, value;
+	int temp;
 
-	value = _pow_recursion(3, (int)size);
-
+/*	value = _pow_recursion(3, (int)size);*/
 	if (size < 2)
 		return;
-	for (interval = (value - 1) / 2; interval > 0; interval /= 2)
+	while (interval < (size / 3))
+		interval = interval * 3 + 1;
+	for (interval = interval; interval > 0; interval /= 3)
 	{
 		for (i = interval; i < size; i += 1)
 		{
 			temp = array[i];
-			for (j = i; j >= interval && array[j - interval] > temp; j-= interval)
+			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
 			{
 				array[j] = array[j - interval];
 			}
